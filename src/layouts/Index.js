afdc43b3ee8c742c1fad5c7c59e12a23278eec63
gkpk30 <guykoomer@hotmail.com>
2022-04-22 08:41:02 -0700

    made a comment

--- src/layouts/Index.js
+++ src/layouts/Index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-
+//wrap each page with Layout tag you want to have this Layout . Using Gatsby's plugin-Layout allows the us of different Layouts for pages depending on design requirements
 function Layout({children}) {
   return (
     <>
